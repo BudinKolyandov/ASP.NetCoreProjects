ba72e42b1f98604cec24c6c86b6514f3726f5e14
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-03-20 19:22:28 +0200

    Added base models and made the index to show all categories

--- ASP.NET Core/Web/ForumSystem.Web/Controllers/HomeController.cs
+++ ASP.NET Core/Web/ForumSystem.Web/Controllers/HomeController.cs
@@ -1,16 +1,33 @@
 ﻿namespace ForumSystem.Web.Controllers
 {
     using System.Diagnostics;
+    using System.Linq;
 
+    using ForumSystem.Data.Common.Repositories;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Services.Mapping;
     using ForumSystem.Web.ViewModels;
-
+    using ForumSystem.Web.ViewModels.Home;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
+        private readonly IDeletableEntityRepository<Category> categoriesRepository;
+
+        public HomeController(IDeletableEntityRepository<Category> categoriesRepository)
+        {
+            this.categoriesRepository = categoriesRepository;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var viewModel = new IndexViewModel();
+            var categories = this.categoriesRepository.All()
+                .To<IndexCategoryViewModel>()
+                .ToList();
+            viewModel.Categories = categories;
+
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
