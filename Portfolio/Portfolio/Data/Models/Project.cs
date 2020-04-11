6639d872f1f4f38c5fed2508c6d8739c6a0a8656
Budin Kolyandov <budin_kolyandov@abv.bg>
2020-04-11 21:09:38 +0300

    Started a protfolio project.

---
+++ Portfolio/Portfolio/Data/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.Data.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string GitHubUrl { get; set; }
+    }
+}
